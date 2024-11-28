2574e3fec59833416086086ef3468feef9d6c53c
mardsonbarboza <mardson49@gmail.com>
2024-11-28 19:10:59 -0300

    feat: implementar método create no User

--- Model/User.js
+++ Model/User.js
@@ -4,6 +4,11 @@ class User{
     async findAll(){
         return await knex.select('*').table('users');
     }
+    async create(nome,email){
+        return await knex.insert({nome,email}).table('users')
+    }
+
+
 }
 
 module.exports = new User();
