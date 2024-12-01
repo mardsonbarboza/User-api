ec0cfa6969b95c21225b00a04d815c8041ce3644
mardsonbarboza <mardson49@gmail.com>
2024-12-01 12:53:22 -0300

    refactor(model User): altera variáveis para padronização e melhora consistência
    - Renomeado "nome" para "name" e "senha" para "password" para padronização.
    - Ajustado o método create para refletir as mudanças nas variáveis.
    - Refatoração para melhorar consistência no código.

--- Model/User.js
+++ Model/User.js
@@ -3,8 +3,8 @@ const knex = require('../Database/Connection');
 class User{
     async findAll(){
         return await knex.select('*').table('users');
-    }async create(nome,email,senha){
-        return await knex.insert({nome,email,senha,created_at:knex.fn.now(), updated_at:knex.fn.now()}).table('users');
+    }async create(name,email,password){
+        return await knex.insert({name,email,password,created_at:knex.fn.now(), updated_at:knex.fn.now()}).table('users');
     }
     async findById(id){
         return await knex.select('*').where({id:id}).table('users');
